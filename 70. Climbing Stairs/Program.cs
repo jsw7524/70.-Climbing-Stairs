6b322e8763240f4a83f2ff2d41e98a4bda3de175
Jason Wu <jsw7524@gmail.com>
2017-11-19 10:36:18 +0800

    dp

--- 70. Climbing Stairs/Program.cs
+++ 70. Climbing Stairs/Program.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace _70.Climbing_Stairs
 {
+
+    public class Solution
+    {
+        private static int[] dp = new int[1000001];
+        public int ClimbStairs(int n)
+        {
+            dp[0] = -1;
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i=3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
     class Program
     {
         static void Main(string[] args)
